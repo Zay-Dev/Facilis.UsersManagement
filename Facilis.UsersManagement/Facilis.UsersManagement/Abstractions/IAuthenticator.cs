b6ffcd92a03f44a393a5d6f5cfb5b1b71c3cfdc0
Zay Lau <contact@zay-dev.com>
2022-02-20 01:09:54 +0800

    created the authenticator interface

---
+++ Facilis.UsersManagement/Facilis.UsersManagement/Abstractions/IAuthenticator.cs
@@ -0,0 +1,13 @@
+﻿using Facilis.UsersManagement.Enums;
+
+namespace Facilis.UsersManagement.Abstractions
+{
+    public delegate void OnAuthenticateFailedEventHandler(object sender, string username, string password);
+
+    public interface IAuthenticator
+    {
+        event OnAuthenticateFailedEventHandler OnAuthenticateFailed;
+
+        LoginFailureTypes TryAuthenticate(string username, string password, out IUser user);
+    }
+}
