6de8c5a01e5638deb57e7f843711f94947e3f61e
Zay Lau <contact@zay-dev.com>
2022-02-20 00:51:03 +0800

    created the login failure types enum

---
+++ Facilis.UsersManagement/Facilis.UsersManagement/Enums/LoginFailureTypes.cs
@@ -0,0 +1,11 @@
+﻿namespace Facilis.UsersManagement.Enums
+{
+    public enum LoginFailureTypes
+    {
+        None,
+        UserNotFound,
+        DisabledUser,
+        LockedUser,
+        PasswordMismatch,
+    }
+}
