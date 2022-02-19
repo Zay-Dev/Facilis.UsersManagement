41c1d9df4b2eea872089ed74300b2a47dfb9e5f9
Zay Lau <contact@zay-dev.com>
2022-02-20 00:43:26 +0800

    created the user interface with its default implementation

---
+++ Facilis.UsersManagement/Facilis.UsersManagement/Abstractions/IUser.cs
@@ -0,0 +1,39 @@
+﻿using Facilis.Core.Abstractions;
+using Facilis.Core.Enums;
+using System;
+
+namespace Facilis.UsersManagement.Abstractions
+{
+    public interface IUser :
+        IEntityWithId,
+        IEntityWithStatus,
+        IEntityWithCreateStamps,
+        IEntityWithUpdateStamps,
+        IPassword
+    {
+        string Username { get; }
+        string Nickname { get; }
+        DateTime? LockedUntilUtc { get; }
+    }
+
+    public class User : IUser
+    {
+        public string Username { get; set; }
+        public string Nickname { get; set; }
+        public DateTime? LockedUntilUtc { get; set; }
+
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public StatusTypes Status { get; set; }
+
+        public string CreatedBy { get; set; }
+        public DateTime CreatedAtUtc { get; set; } = DateTime.UtcNow;
+
+        public string UpdatedBy { get; set; }
+        public DateTime UpdatedAtUtc { get; set; } = DateTime.UtcNow;
+
+        public string HashingMethod { get; set; }
+        public string HashedPassword { get; set; }
+        public string PasswordSalt { get; set; }
+        public int PasswordIterated { get; set; }
+    }
+}
