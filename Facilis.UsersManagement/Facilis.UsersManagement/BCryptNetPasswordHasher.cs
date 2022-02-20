4e4ab7b354b9a4844af894e57de6933e26d1f419
Zay Lau <contact@zay-dev.com>
2022-02-20 10:30:07 +0800

    provided the default password hasher that consumes the BCrypt.Net

---
+++ Facilis.UsersManagement/Facilis.UsersManagement/BCryptNetPasswordHasher.cs
@@ -0,0 +1,32 @@
+﻿using Facilis.Core.Abstractions;
+
+namespace Facilis.UsersManagement
+{
+    public class BCryptNetPasswordHasher : IPasswordHasher
+    {
+        public IPassword Hash(string value)
+        {
+            var salt = BCrypt.Net.BCrypt.GenerateSalt();
+
+            return new Password()
+            {
+                HashingMethod = nameof(BCrypt),
+                HashedPassword = BCrypt.Net.BCrypt.HashPassword(value, salt),
+                PasswordSalt = salt
+            };
+        }
+
+        public bool Verify(IPassword hashed, string value)
+        {
+            return BCrypt.Net.BCrypt.Verify(value, hashed.HashedPassword);
+        }
+
+        private class Password : IPassword
+        {
+            public string HashingMethod { get; set; }
+            public string HashedPassword { get; set; }
+            public string PasswordSalt { get; set; }
+            public int PasswordIterated { get; set; }
+        }
+    }
+}
