00641ea2c913ba4aede87e1b7746234989595efe
Zay Lau <contact@zay-dev.com>
2022-02-20 10:37:33 +0800

    created the dotnet 5 test project

---
+++ Facilis.UsersManagement/Facilis.UsersManagement.Tests/Authenticate.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Facilis.UsersManagement.Tests
+{
+    public class Authenticate
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
