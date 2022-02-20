7b83bd187a0f055d826b0f6444fd37ad79fd1093
Zay Lau <contact@zay-dev.com>
2022-02-20 13:24:52 +0800

    initialized the app db context

---
+++ Facilis.UsersManagement/Facilis.UsersManagement.SampleApp/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Facilis.UsersManagement.Abstractions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Facilis.UsersManagement.SampleApp
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        #region Constructor(s)
+
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        #endregion Constructor(s)
+    }
+}
