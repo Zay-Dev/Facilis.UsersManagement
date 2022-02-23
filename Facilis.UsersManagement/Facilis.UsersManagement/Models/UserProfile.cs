a10536ca1f8e7556d5cbc4556411f5bbf7751699
Zay Lau <contact@zay-dev.com>
2022-02-23 08:59:33 +0800

    added the immutable property attribute to the default user profile's last sign in at utc property

--- Facilis.UsersManagement/Facilis.UsersManagement/Models/UserProfile.cs
+++ Facilis.UsersManagement/Facilis.UsersManagement/Models/UserProfile.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using Facilis.Core.Attributes;
+using System;
 
 namespace Facilis.UsersManagement.Models
 {
     public class UserProfile
     {
         public string[] Roles { get; set; }
+
+        [Immutable]
         public DateTime LastSignInAtUtc { get; set; }
     }
 }
