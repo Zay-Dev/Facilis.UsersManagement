1bef52b3397cf2fe65512a2981440fe7be3ab74c
Zay Lau <contact@zay-dev.com>
2022-03-06 23:37:06 +0800

    enhanced the authentication history with the method name and information properties

--- Facilis.UsersManagement/Facilis.UsersManagement/Abstractions/IAuthenticationHistory.cs
+++ Facilis.UsersManagement/Facilis.UsersManagement/Abstractions/IAuthenticationHistory.cs
@@ -10,16 +10,20 @@ namespace Facilis.UsersManagement.Abstractions
         IUserRelatedEntity
     {
         LoginFailureTypes Failure { get; }
+        string MethodName { get; }
+        string Information { get; }
     }
 
     public class AuthenticationHistory : IAuthenticationHistory
     {
-        public string Id { get; set; }
+        public string Id { get; set; } = Guid.NewGuid().ToString();
 
         public string CreatedBy { get; set; }
         public DateTime CreatedAtUtc { get; set; }
 
         public string UserId { get; set; }
         public LoginFailureTypes Failure { get; set; }
+        public string MethodName { get; set; }
+        public string Information { get; set; }
     }
 }
