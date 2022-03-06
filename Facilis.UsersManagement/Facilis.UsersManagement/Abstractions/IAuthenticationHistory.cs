62f8427764eea8c21bce4d471f3b458370cc65b5
Zay Lau <contact@zay-dev.com>
2022-03-06 23:12:18 +0800

    created the authentication history interface with its default implementation

---
+++ Facilis.UsersManagement/Facilis.UsersManagement/Abstractions/IAuthenticationHistory.cs
@@ -0,0 +1,25 @@
+﻿using Facilis.Core.Abstractions;
+using Facilis.UsersManagement.Enums;
+using System;
+
+namespace Facilis.UsersManagement.Abstractions
+{
+    public interface IAuthenticationHistory :
+        IEntityWithId,
+        IEntityWithCreateStamps,
+        IUserRelatedEntity
+    {
+        LoginFailureTypes Failure { get; }
+    }
+
+    public class AuthenticationHistory : IAuthenticationHistory
+    {
+        public string Id { get; set; }
+
+        public string CreatedBy { get; set; }
+        public DateTime CreatedAtUtc { get; set; }
+
+        public string UserId { get; set; }
+        public LoginFailureTypes Failure { get; set; }
+    }
+}
