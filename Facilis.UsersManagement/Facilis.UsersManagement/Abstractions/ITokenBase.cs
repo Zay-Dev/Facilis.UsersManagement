eb1c4dfb6fe1f889b72f2a5585a9997f0db94efa
Zay Lau <contact@zay-dev.com>
2022-03-06 21:56:08 +0800

    created the token base and user token interfaces with their default implementation

---
+++ Facilis.UsersManagement/Facilis.UsersManagement/Abstractions/ITokenBase.cs
@@ -0,0 +1,21 @@
+﻿namespace Facilis.UsersManagement.Abstractions
+{
+    public interface ITokenBase : IAuthenticateInput
+    {
+        string TokenId { get; }
+        string Value { get; }
+
+        IUserToken UserToken { get; set; }
+    }
+
+    public class TokenBase : ITokenBase
+    {
+        public const string METHOD_NAME = nameof(TokenBase);
+
+        public string MethodName => METHOD_NAME;
+        public string TokenId { get; set; }
+        public string Value { get; set; }
+
+        public IUserToken UserToken { get; set; }
+    }
+}
---
+++ Facilis.UsersManagement/Facilis.UsersManagement/Abstractions/IUserToken.cs
@@ -0,0 +1,33 @@
+﻿using Facilis.Core.Abstractions;
+using Facilis.Core.Enums;
+using System;
+
+namespace Facilis.UsersManagement.Abstractions
+{
+    public interface IUserToken :
+        IEntityWithId,
+        IEntityWithStatus,
+        IEntityWithCreateStamps,
+        IPassword,
+        IUserRelatedEntity,
+        IEntityWithExpiration
+    {
+    }
+
+    public class UserToken : IUserToken
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        public StatusTypes Status { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedAtUtc { get; set; } = DateTime.UtcNow;
+
+        public string HashingMethod { get; set; }
+        public string HashedPassword { get; set; }
+        public string PasswordSalt { get; set; }
+        public int PasswordIterated { get; set; }
+
+        public string UserId { get; set; }
+        public DateTime ExpiredAtUtc { get; set; }
+    }
+}
