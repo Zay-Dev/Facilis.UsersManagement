ee01149acb35129f76d636156cfa33415bb93763
Zay Lau <contact@zay-dev.com>
2022-03-13 22:33:30 +0800

    enhanced the user profile model for the country code, phone number and avatar properties

--- Facilis.UsersManagement/Facilis.UsersManagement/Models/UserProfile.cs
+++ Facilis.UsersManagement/Facilis.UsersManagement/Models/UserProfile.cs
@@ -5,8 +5,11 @@ namespace Facilis.UsersManagement.Models
 {
     public class UserProfile
     {
+        public string Avatar { get; set; }
         public string Nickname { get; set; }
         public string Email { get; set; }
+        public string CountryCode { get; set; }
+        public string PhoneNumber { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
 
