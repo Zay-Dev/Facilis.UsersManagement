0d23ce7a03e9d94fd2d5158cec77e6199d421cbf
Zay Lau <contact@zay-dev.com>
2022-03-28 10:52:03 +0800

    created the invitation interface with its default implementation

---
+++ Facilis.UsersManagement/Facilis.UsersManagement/Abstractions/IInvitation.cs
@@ -0,0 +1,31 @@
+﻿using Facilis.Core.Abstractions;
+using Facilis.Core.Enums;
+using System;
+
+namespace Facilis.UsersManagement.Abstractions
+{
+    public interface IInvitation :
+        IEntityWithId,
+        IUserRelatedEntity,
+        IEntityWithStatus,
+        IEntityWithCreateStamps
+    {
+        string InvitationType { get; }
+        string SentBy { get; }
+        bool IsAccepted { get; }
+    }
+
+    public class Invitation : IInvitation
+    {
+        public string InvitationType { get; set; }
+        public string SentBy { get; set; }
+        public bool IsAccepted { get; set; }
+
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string UserId { get; set; }
+
+        public StatusTypes Status { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedAtUtc { get; set; } = DateTime.UtcNow;
+    }
+}
