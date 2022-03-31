e096ee462ee97dde7389b396b0536f7ba356a6ed
Zay Lau <contact@zay-dev.com>
2022-03-31 15:34:17 +0800

    created the invitation helper

---
+++ Facilis.UsersManagement/Facilis.UsersManagement/Helpers/InvitationHelper.cs
@@ -0,0 +1,43 @@
+﻿using Facilis.Core.Abstractions;
+using Facilis.UsersManagement.Abstractions;
+using System.Linq;
+
+namespace Facilis.UsersManagement.Helpers
+{
+    public static class InvitationHelper
+    {
+        public static IQueryable<T> FindSentInvitations<T>(
+            this IEntitiesWithId<T> entities,
+            string type,
+            string userId,
+            bool onlyAccepted = false
+        ) where T : IInvitation
+        {
+            return entities
+                .Where(entity => entity.InvitationType == type &&
+                    entity.SentBy == userId &&
+                    (
+                        onlyAccepted == false ||
+                        entity.IsAccepted
+                    )
+                );
+        }
+
+        public static IQueryable<T> FindReceivedInvitations<T>(
+            this IEntitiesWithId<T> entities,
+            string type,
+            string userId,
+            bool onlyAccepted = false
+        ) where T : IInvitation
+        {
+            return entities
+                .Where(entity => entity.InvitationType == type &&
+                    entity.UserId == userId &&
+                    (
+                        onlyAccepted == false ||
+                        entity.IsAccepted
+                    )
+                );
+        }
+    }
+}
